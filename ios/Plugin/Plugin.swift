020a945c0c22b4c6fc521e71b51d899f1f30cd35
Antonio Romero <a.romero@docline.es>
2021-04-09 14:34:27 +0200

    Fixed issue with alpha

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -405,7 +405,7 @@ extension UIColor {
             var hexColor = String(hex[start...])
             
             if hexColor.count == 6 {
-                hexColor = "\(hexColor)FF"
+                hexColor = "FF\(hexColor)"
             }
             
             if hexColor.count == 8 {
@@ -414,11 +414,11 @@ extension UIColor {
 
                 if scanner.scanHexInt64(&hexNumber) {
                     let r, g, b, a: CGFloat
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-
+                    a = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    r = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    g = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    b = CGFloat(hexNumber & 0x000000ff) / 255
+                    
                     self.init(red: r, green: g, blue: b, alpha: a)
                     return
                 }
